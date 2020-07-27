505ee84da0582a9be14ddb3085d5fe495419d4da
kingspp <kingspprathyush@gmail.com>
2020-07-27 10:07:25 -0400

    Updated logging format - Following standard format

--- main.cpp
+++ main.cpp
@@ -7,52 +7,72 @@
 #include <QFile>
 #include <QDir>
 #include <usoundutils.h>
+#include <logfilewatcherthread.h>
+#include <QDateTime>
 
 QString logFilePath = QDir::tempPath()+QString("/USoundApp-%1.log").arg(generateTimeStamp().c_str());
+QString tempString = "";
 
 void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     QByteArray localMsg = msg.toLocal8Bit();
     QString txt;
 
+
+
     switch (type) {
     case QtDebugMsg:
-        txt = QString("Debug: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
-        fprintf(stderr, txt.toLatin1().data());
+        txt = QString("%1 - DEBUG - %3:%4:%5 - %2").arg(QDateTime::currentDateTime().toString()).arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        fprintf(stdout, "%s\n", txt.toLatin1().data());
         break;
     case QtInfoMsg:
-        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        txt = QString("%1 - INFO - %2").arg(QDateTime::currentDateTime().toString()).arg(localMsg.constData());
+        fprintf(stdout, "%s\n", txt.toLatin1().data());
         break;
     case QtWarningMsg:
-        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        txt = QString("%1 - WARNING - %2").arg(QDateTime::currentDateTime().toString()).arg(localMsg.constData());
+        fprintf(stdout, "%s\n", txt.toLatin1().data());
         break;
     case QtCriticalMsg:
-        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        txt = QString("%1 - CRITICAL - %3:%4:%5 - %2").arg(QDateTime::currentDateTime().toString()).arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        fprintf(stderr, "%s\n", txt.toLatin1().data());
         break;
     case QtFatalMsg:
-        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        abort();
+        txt = QString("%1 - FATAL - %3:%4:%5 - %2").arg(QDateTime::currentDateTime().toString()).arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        fprintf(stderr, "%s\n", txt.toLatin1().data());
+        break;
     }
+
      //todo: Prathyush SP -> Set settings for log file path
     QFile outFile(logFilePath);
     outFile.open(QIODevice::WriteOnly | QIODevice::Append);
     QTextStream ts(&outFile);
     ts << txt << endl;
+
+    // Push to Message Box
+    if(Homescreen::globalMessageBox != 0){
+        Homescreen::globalMessageBox->appendPlainText(tempString+txt);
+        tempString="";
+    }
+    else{
+        tempString+=txt+'\n';
+    }
+
+
 }
 
 int main(int argc, char *argv[])
 {
     try {
-        fprintf(stdout, "Logging to file: %s", logFilePath.toStdString().c_str());
+        fprintf(stdout, "Logging to file: %s", (logFilePath.toStdString()+"\n").c_str());
         qInstallMessageHandler(myMessageOutput); // Install the handler
+        qDebug() << QString("Logging to file: %1").arg(logFilePath);
         QApplication a(argc, argv);
         Homescreen w;
-
         w.setWindowIcon(QIcon("://icons/wpi_logo.ico"));
         w.show();
         return a.exec();
     } catch (std::exception &e) {
-
         qDebug() << e.what();
     }
 
