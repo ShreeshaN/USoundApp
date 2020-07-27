b9a3b237f7ebbd5015b485ef71e022576de7c4a1
kingspp <kingspprathyush@gmail.com>
2020-07-27 01:39:35 -0400

    Added usoundutils for common functionality

---
+++ usoundutils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <QDateTime>
+#endif // UTILS_H
+
+static std::string generateTimeStamp(){
+    QDateTime currentDateTime = QDateTime::currentDateTime();
+    return std::to_string(currentDateTime.toTime_t());
+}
