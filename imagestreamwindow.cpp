e38c2dbad7d5df6abb046ead9303012fe29d34f0
kingspp <kingspprathyush@gmail.com>
2020-07-27 11:24:07 -0400

    OS Independent Sleep

--- imagestreamwindow.cpp
+++ imagestreamwindow.cpp
@@ -109,8 +109,10 @@ void ImageStreamWindow::setupCameraWindow()
         // will query the camera and update cameracontrols obj
         this->imageAcquisitionThread->getCameraControls().setAutoExposure(autoExposureCheckbox->isChecked());
         imageAcquisitionThread->setValueForParam(HalconCameraParameterNames::AUTOEXPOSURE,autoExposureCheckbox->isChecked()?"Once":"Off");
-        Sleep(500); // Waiting for the hardware to update exposure time based on Autoexposure
+        mssleep(500);
+//        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Waiting for the hardware to update exposure time based on Autoexposure
         updateCameraParametersAndDisplay();
+
 
     });
 
--- usoundutils.h
+++ usoundutils.h
@@ -2,9 +2,13 @@
 #define UTILS_H
 #include <QDateTime>
 #endif // UTILS_H
+#include<thread>
 
 static std::string generateTimeStamp(){
     QDateTime currentDateTime = QDateTime::currentDateTime();
     return std::to_string(currentDateTime.toTime_t());
 }
 
+static void mssleep(int ms){
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
